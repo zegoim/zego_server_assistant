8dfcafbdcb06f525580bb87b713023d2082a8d8e
lancelot6806 <lancelot6806@gmail.com>
2022-04-18 13:31:41 +0800

    change payload demo info

--- token/python/token04/test/test.py
+++ token/python/token04/test/test.py
@@ -8,5 +8,8 @@ sys.path.append(root_path)
 from src import token04
 
 if __name__ == '__main__':
-    token_info = token04.generate_token04(1, "demo", "12345678900987654321123456789012", 3600, "payload")
+    payload = {
+        "demoPayload": ""
+    }
+    token_info = token04.generate_token04(1, "demo", "12345678900987654321123456789012", 3600, json.dumps(payload))
     print([token_info.token, token_info.error_code, token_info.error_message])
